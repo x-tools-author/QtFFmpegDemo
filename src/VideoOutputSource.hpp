c642c9aaf8e3f39e392b839432e86ee90fb405cf
wuhai <wuhai@cuav.net>
2020-03-13 09:24:49 +0800

    add files

---
+++ src/VideoOutputSource.hpp
@@ -0,0 +1,18 @@
+﻿#ifndef TTVIDEOIMAGEPROVIDER_H
+#define TTVIDEOIMAGEPROVIDER_H
+
+#include <QQuickPaintedItem>
+#include <QImage>
+
+class QuickPaintedItem:public QQuickPaintedItem
+{
+    Q_OBJECT
+public:
+    QuickPaintedItem(QQuickItem *parent = nullptr);
+    void paint(QPainter *painter) override;
+    void setImage(QImage image);
+private:
+    QImage videoFrame;
+};
+
+#endif
